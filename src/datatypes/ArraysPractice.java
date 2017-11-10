ab34c00658f09922a5fb460eb4e5254c0965c1d2
esha <kramki10@yahoo.com>
2017-11-09 19:04:31 -0600

    <change comments>

---
+++ src/datatypes/ArraysPractice.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class ArraysPractice {
+
+    public static void main(String[] args) {
+
+
+        String[] strArray = {"Esha", "Shreya", "Rama", "Suja"};
+
+        for (String es: strArray){
+
+            System.out.println("The names of the people in my family are " + es);
+        }
+
+    }
+}
---
+++ src/datatypes/StringHomeWork.java
@@ -0,0 +1,41 @@
+package datatypes;
+
+public class StringHomeWork {
+
+    public static void main(String[] args) {
+
+        String str, str1;
+
+        str = "hello";
+        str1 = "world";
+
+        System.out.println(str);
+        System.out.println(str1);
+
+        System.out.println(str+str1);
+
+        System.out.println(str.substring(0,3));
+        System.out.println(str.replace('l', 'i'));
+
+        System.out.println("The length of hello is " + str.length());
+
+        System.out.println(str.contains("o"));
+
+        String str2 = "car-bike-trailer";
+
+        System.out.println(str2.split("-"));
+
+        String[] strings = str2.split("-");
+
+        for(String string : strings){
+
+            System.out.println("Split value is " + str2);
+        }
+
+
+
+
+
+
+    }
+}
