9ba37a305e390452b21afd0b1d992b06796a182a
esha <kramki10@yahoo.com>
2017-11-14 19:48:50 -0600

    added fiveTimesBack

---
+++ src/forloop/ForLoopPractice.java
@@ -0,0 +1,16 @@
+package forloop;
+
+public class ForLoopPractice {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 5; i++){
+            System.out.println("i equals " + i);
+        }
+
+        for (int i = 5; i >=1; i--){
+            System.out.println("Going backwards, i equals " + i);
+        }
+
+    }
+}
